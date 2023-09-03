f8fbd13e1b6c9836adbb1fb36be4d863fd740b2a
SheyMansur <87302737+SheyMansur@users.noreply.github.com>
2023-09-02 18:16:12 -0700

    Time: 13 ms (27.22%) | Memory: 46.6 MB (90.55%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,9 +3,13 @@ class Solution {
         Map<Integer, Integer> count = new HashMap<>();
         int index = 0;
         int max = 0;
+        int cap = nums.length / 2;
         for (int i = 0; i < nums.length; i++) {
             if (count.containsKey(nums[i])) {
                 int curr = count.get(nums[i]) + 1;
+                if (curr > cap) {
+                    return nums[i];
+                }
                 if (curr > max) {
                     max = curr;
                     index = i;
