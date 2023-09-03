9a8bbe17896da21615753a8ea7a9eafebd82f882
SheyMansur <87302737+SheyMansur@users.noreply.github.com>
2023-09-03 15:17:36 -0700

    Time: 0 ms (100.00%) | Memory: 56.7 MB (6.20%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,54 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        // void reverseRange(int i, int j) {
-        //     int temp = nums[i]
-        //     while (j > i) {
-        //         nums[i] = nums[j];
-        //         nums[j] = temp;
-        //         i++;
-        //         temp = nums[i];
-        //         j--;
-        //     }
-        // }
-        // int m = nums.length - 1;
-        // reverseRange(0, m);
-        // reverseRange(0, k - 1);
-        // reverseRange(k, m);
         k = (k % nums.length);
-        int i = 0;
-        int j = nums.length - 1;
-        if (j == 0) {
-            return;
-        }
-        int temp;
-        while (j > i) {
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            temp = nums[i];
-            j--;
-        }
-        i = 0;
-        j = k - 1;
-        while (j > i) {
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
-        i = k;
-        j = nums.length - 1;
-        while (j > i) {
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
-
+        int m = nums.length - 1;
+        reverseRange(0, m, nums);
+        reverseRange(0, k - 1, nums);
+        reverseRange(k, m, nums);
     }
+
+    public void reverseRange(int i, int j, int[] nums) {
+            while (j > i) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+        }
 
     
 }
